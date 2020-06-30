90850d158742d5eb70f0ed5111b5bbdee96efcaa
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2020-06-30 14:06:28 +0200

    Added SpeedConverter with tests

---
+++ UnitConverter.UnitTests/SpeedConverterTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LorinthsLair.UnitConverter.UnitTests
+{
+    [TestFixture]
+    public class SpeedConverterTests
+    {
+        [Test]
+        [TestCase(1, 3.6)]
+        public void ConvertMetersPerSecondToKilometersPerHour_WhenCalled_ReturnsSpeedInKilometersPerHour(double metersPerSecond, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ConvertMetersPerSecondToKilometersPerHour(metersPerSecond));
+        }
+
+        [Test]
+        [TestCase(3.6, 1)]
+        public void ConvertKilometersPerHourToMetersPerSecond_WhenCalled_ReturnsSpeedInMetersPerSecond(double kilometersPerHour, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ConvertKilometersPerHourToMetersPerSecond(kilometersPerHour));
+        }
+
+        [Test]
+        [TestCase(1.609344, 1.0)]
+        public void ConvertKilometersPerHourToMilesPerHour_WhenCalled_ReturnsSpeedInMilesPerHour(double kilometersPerHour, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ConvertKilometersPerHourToMilesPerHour(kilometersPerHour));
+        }
+
+        [Test]
+        [TestCase(1.0, 1.609344)]
+        public void ConvertMilesPerHourToKilometersPerHour_WhenCalled_ReturnsSpeedInKilometersPerHour(double milesPerHour, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ConvertMilesPerHourToKilometersPerHour(milesPerHour));
+        }
+
+        [Test]
+        [TestCase("2.54c")]
+        [TestCase("Lorem Ipsum")]
+        [TestCase("4'11")]
+        [TestCase("")]
+        public void ParseMetersPerSecondToKilometersPerHour_WhenCalledWithNotParseableArgument_ThrowsFormatException(string speedInMetersPerSecond)
+        {
+            Assert.Throws<FormatException>(() => SpeedConverter.ParseMetersPerSecondToKilometersPerHour(speedInMetersPerSecond));
+        }
+
+        [Test]
+        [TestCase("1.0m/sec", "3.6km/h", 1)]
+        [TestCase("1m/s", "3.60km/h", 2)]
+        [TestCase("1", "3km/h", 0)]
+        public void ParseMetersPerSecondToKilometersPerHour_WhenCalled_ReturnsFormatedSpeed(string speedInMetersPerSecond, string expectedResult, int decimalPlaces)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ParseMetersPerSecondToKilometersPerHour(speedInMetersPerSecond, decimalPlaces));
+        }
+
+        [Test]
+        [TestCase("2.54c")]
+        [TestCase("Lorem Ipsum")]
+        [TestCase("4'11")]
+        [TestCase("")]
+        public void ParseKilometersPerHourToMetersPerSecond_WhenCalledWithNotParseableArgument_ThrowsFormatException(string speedInKilometersPerHour)
+        {
+            Assert.Throws<FormatException>(() => SpeedConverter.ParseKilometersPerHourToMetersPerSecond(speedInKilometersPerHour));
+        }
+
+        [Test]
+        [TestCase("3.60kmh", "1.0m/s", 1)]
+        [TestCase("3.6km/h", "1.00m/s", 2)]
+        [TestCase("3.6", "1m/s", 0)]
+        public void ParseKilometersPerHourToMetersPerSecond_WhenCalled_ReturnsFormatedSpeed(string speedInKilometersPerHour, string expectedResult, int decimalPlaces)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ParseKilometersPerHourToMetersPerSecond(speedInKilometersPerHour, decimalPlaces));
+        }
+
+        [Test]
+        [TestCase("2.54c")]
+        [TestCase("Lorem Ipsum")]
+        [TestCase("4'11")]
+        [TestCase("")]
+        public void ParseKilometersPerHourToMilesPerHour_WhenCalledWithNotParseableArgument_ThrowsFormatException(string speedInKilometersPerHour)
+        {
+            Assert.Throws<FormatException>(() => SpeedConverter.ParseKilometersPerHourToMilesPerHour(speedInKilometersPerHour));
+        }
+
+        [Test]
+        [TestCase("1.609344km/h", "1.0MPH", 1)]
+        [TestCase("1.609344km/h", "1MPH", 0)]
+        [TestCase("1.609344KMH", "1.0MPH", 1)]
+        public void ParseKilometersPerHourToMilesPerHour_WhenCalled_ReturnsFormatedSpeed(string speedInKilometersPerHour, string expectedResult, int decimalPlaces)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ParseKilometersPerHourToMilesPerHour(speedInKilometersPerHour, decimalPlaces));
+        }
+
+        [Test]
+        [TestCase("2.54c")]
+        [TestCase("Lorem Ipsum")]
+        [TestCase("2cm")]
+        [TestCase("")]
+        public void ParseMilesPerHourToKilometersPerHour_WhenCalledWithNotParseableArgument_ThrowsFormatException(string speedInMilesPerHour)
+        {
+            Assert.Throws<FormatException>(() => SpeedConverter.ParseMilesPerHourToKilometersPerHour(speedInMilesPerHour));
+        }
+
+        [Test]
+        [TestCase("1.0", "1.609344km/h", 6)]
+        [TestCase("1.0mi/h", "1.609344km/h", 6)]
+        [TestCase("1.0MPH", "1.609km/h", 3)]
+        public void ParseMilesPerHourToKilometersPerHour_WhenCalled_ReturnsFormatedSpeed(string speedInMilesPerHour, string expectedResult, int decimalPlaces)
+        {
+            Assert.AreEqual(expectedResult, SpeedConverter.ParseMilesPerHourToKilometersPerHour(speedInMilesPerHour, decimalPlaces));
+        }
+    }
+}
---
+++ UnitConverter/SpeedConverter.cs
@@ -0,0 +1,145 @@
+﻿namespace LorinthsLair.UnitConverter
+{
+    /// <summary>
+    /// Speed units convertions
+    /// </summary>
+    public class SpeedConverter
+    {
+        /// <summary>
+        /// Converts given speed in meters per second to kilometers per hour.
+        /// </summary>
+        /// <param name="metersPerSecond">
+        /// Speed in meters per second to convert.
+        /// </param>
+        /// <returns>
+        /// Converted speed to kilometers per hour.
+        /// </returns>
+        public static double ConvertMetersPerSecondToKilometersPerHour(double metersPerSecond)
+        {
+            return metersPerSecond * 3600 / 1000;
+        }
+
+        /// <summary>
+        /// Converts given speed in kilometers per hour to meters per second.
+        /// </summary>
+        /// <param name="kilometersPerHour">
+        /// Speed in kilometers per hour to convert.
+        /// </param>
+        /// <returns>
+        /// Converted speed to meters per second.
+        /// </returns>
+        public static double ConvertKilometersPerHourToMetersPerSecond(double kilometersPerHour)
+        {
+            return kilometersPerHour * 1000 / 3600;
+        }
+
+        /// <summary>
+        /// Converts given speed in kilometers per hour to miles per hour.
+        /// </summary>
+        /// <param name="kilometersPerHour">
+        /// Speed in kilometers per hour to convert.
+        /// </param>
+        /// <returns>
+        /// Converted speed to miles per hour.
+        /// </returns>
+        public static double ConvertKilometersPerHourToMilesPerHour(double kilometersPerHour)
+        {
+            // Because unit of time stays the same, I can only convert length unit
+            return LengthConverter.ConvertKilometersToMiles(kilometersPerHour);
+        }
+
+        /// <summary>
+        /// Converts given speed in miles per hour to kilometers per hour.
+        /// </summary>
+        /// <param name="milesPerHour">
+        /// Speed in miles per hour to convert.
+        /// </param>
+        /// <returns>
+        /// Converted speed to kilometers per hour.
+        /// </returns>
+        public static double ConvertMilesPerHourToKilometersPerHour(double milesPerHour)
+        {
+            return LengthConverter.ConvertMilesToKilometers(milesPerHour);
+        }
+
+        /// <summary>
+        /// Parse speed in meters per second from string to kilometers per hour.
+        /// </summary>
+        /// <param name="speedInMetersPerSecond">
+        /// Speed in meters per second.
+        /// </param>
+        /// <param name="decimalPlaces">
+        /// Desired accuracy.
+        /// </param>
+        /// <returns>
+        /// Formatted speed in kilometers per hour.
+        /// </returns>
+        public static string ParseMetersPerSecondToKilometersPerHour(string speedInMetersPerSecond, int decimalPlaces = 0)
+        {
+            double metersPerSecond = double.Parse(speedInMetersPerSecond.ToLower().Replace("m/sec", "").Replace("m/s", "").Replace('.', ',').Trim());
+            double kilometersPerHour = ConvertMetersPerSecondToKilometersPerHour(metersPerSecond);
+
+            return $"{Utilities.FormatDouble(kilometersPerHour, decimalPlaces)}km/h";
+        }
+
+        /// <summary>
+        /// Parse speed in kilometers per hour from string to meters per second.
+        /// </summary>
+        /// <param name="speedInKilometersPerHour">
+        /// Speed in kilometers per hour.
+        /// </param>
+        /// <param name="decimalPlaces">
+        /// Desired accuracy.
+        /// </param>
+        /// <returns>
+        /// Formatted speed in meters per second.
+        /// </returns>
+        public static string ParseKilometersPerHourToMetersPerSecond(string speedInKilometersPerHour, int decimalPlaces = 0)
+        {
+            double kilometersPerHour = double.Parse(speedInKilometersPerHour.ToLower().Replace("km/h", "").Replace("kmh", "").Replace('.', ',').Trim());
+            double metersPerSecond = ConvertKilometersPerHourToMetersPerSecond(kilometersPerHour);
+
+            return $"{Utilities.FormatDouble(metersPerSecond, decimalPlaces)}m/s";
+        }
+
+        /// <summary>
+        /// Parse speed in kilometers per hour from string to miles per hour.
+        /// </summary>
+        /// <param name="speedInKilometersPerHour">
+        /// Speed in kilometers per hour.
+        /// </param>
+        /// <param name="decimalPlaces">
+        /// Desired accuracy.
+        /// </param>
+        /// <returns>
+        /// Formatted speed in miles per hour.
+        /// </returns>
+        public static string ParseKilometersPerHourToMilesPerHour(string speedInKilometersPerHour, int decimalPlaces = 0)
+        {
+            double kilometersPerHour = double.Parse(speedInKilometersPerHour.ToLower().Replace("km/h", "").Replace("kmh", "").Replace('.', ',').Trim());
+            double milesPerHour = ConvertKilometersPerHourToMilesPerHour(kilometersPerHour);
+
+            return $"{Utilities.FormatDouble(milesPerHour, decimalPlaces)}MPH";
+        }
+
+        /// <summary>
+        /// Parse speed in miles per hour from string to kilometers per hour.
+        /// </summary>
+        /// <param name="speedInMilesPerHour">
+        /// Speed in miles per hour.
+        /// </param>
+        /// <param name="decimalPlaces">
+        /// Desired accuracy.
+        /// </param>
+        /// <returns>
+        /// Formatted speed in kilometers per hour.
+        /// </returns>
+        public static string ParseMilesPerHourToKilometersPerHour(string speedInMilesPerHour, int decimalPlaces = 0)
+        {
+            double milesPerHour = double.Parse(speedInMilesPerHour.ToLower().Replace("mph", "").Replace("mi/h", "").Replace('.', ',').Trim());
+            double kilometersPerHour = ConvertMilesPerHourToKilometersPerHour(milesPerHour);
+
+            return $"{Utilities.FormatDouble(kilometersPerHour, decimalPlaces)}km/h";
+        }
+    }
+}
