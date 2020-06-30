f255f1b87c486d5a5f0e9eee93f06e6055e6f624
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2020-06-30 11:26:22 +0200

    Added TimeConverter with tests

---
+++ UnitConverter.UnitTests/TimeConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LorinthsLair.UnitConverter.UnitTests
+{
+    [TestFixture]
+    public class TimeConverterTests
+    {
+        [Test]
+        [TestCase(60, 1)]
+        [TestCase(15.0, 0.25)]
+        public void ConvertSecondsToMinutes_WhenCalled_ReturnsTimeInMinutes(double seconds, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, TimeConverter.ConvertSecondsToMinutes(seconds));
+        }
+
+        [Test]
+        [TestCase(1.0, 60)]
+        [TestCase(0.25, 15.0)]
+        public void ConvertMinutesToSeconds_WhenCalled_ReturnsTimeInSeconds(double minutes, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, TimeConverter.ConvertMinutesToSeconds(minutes));
+        }
+
+        [Test]
+        [TestCase(60, 1)]
+        [TestCase(15.0, 0.25)]
+        public void ConvertMinutesToHours_WhenCalled_ReturnsTimeInHours(double minutes, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, TimeConverter.ConvertMinutesToHours(minutes));
+        }
+
+        [Test]
+        [TestCase(1.0, 60)]
+        [TestCase(0.25, 15.0)]
+        public void ConvertHoursToMinutes_WhenCalled_ReturnsTimeInMinutes(double hours, double expectedResult)
+        {
+            Assert.AreEqual(expectedResult, TimeConverter.ConvertHoursToMinutes(hours));
+        }
+    }
+}
---
+++ UnitConverter/TimeConverter.cs
@@ -0,0 +1,64 @@
+﻿namespace LorinthsLair.UnitConverter
+{
+    /// <summary>
+    /// Time units convertions
+    /// </summary>
+    public class TimeConverter
+    {
+        /// <summary>
+        /// Converts given time in seconds to minutes.
+        /// </summary>
+        /// <param name="seconds">
+        /// Time in seconds to convert.
+        /// </param>
+        /// <returns>
+        /// Converted time to minutes.
+        /// </returns>
+        public static double ConvertSecondsToMinutes(double seconds)
+        {
+            return seconds / 60;
+        }
+
+        /// <summary>
+        /// Converts given time in minutes to seconds.
+        /// </summary>
+        /// <param name="minutes">
+        /// Time in minutes to convert.
+        /// </param>
+        /// <returns>
+        /// Converted time to seconds.
+        /// </returns>
+        public static double ConvertMinutesToSeconds(double minutes)
+        {
+            return minutes * 60;
+        }
+
+        /// <summary>
+        /// Converts given time in minutes to hours.
+        /// </summary>
+        /// <param name="minutes">
+        /// Time in minutes to convert.
+        /// </param>
+        /// <returns>
+        /// Converted time to hours.
+        /// </returns>
+        public static double ConvertMinutesToHours(double minutes)
+        {
+            return minutes / 60;
+        }
+
+        /// <summary>
+        /// Converts given time in hours to minutes.
+        /// </summary>
+        /// <param name="hours">
+        /// Time in hours to convert.
+        /// </param>
+        /// <returns>
+        /// Converted time to minutes.
+        /// </returns>
+        public static double ConvertHoursToMinutes(double hours)
+        {
+            return hours * 60;
+        }
+    }
+}
